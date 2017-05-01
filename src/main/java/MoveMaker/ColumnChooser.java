eb32e9434665e822e642448ccd560bbe19b3dd09
itamargreen <itamar.green@gmail.com>
2017-05-01 17:48:39 +0300

    added Early stopping to column chooser

--- src/main/java/MoveMaker/ColumnChooser.java
+++ src/main/java/MoveMaker/ColumnChooser.java
@@ -3,7 +3,15 @@ package MoveMaker;
 import GameObjects.BoardWinPair;
 import GameObjects.State;
 import org.deeplearning4j.datasets.iterator.impl.ListDataSetIterator;
+import org.deeplearning4j.earlystopping.EarlyStoppingConfiguration;
+import org.deeplearning4j.earlystopping.EarlyStoppingResult;
+import org.deeplearning4j.earlystopping.saver.LocalFileModelSaver;
+import org.deeplearning4j.earlystopping.scorecalc.DataSetLossCalculator;
+import org.deeplearning4j.earlystopping.termination.MaxEpochsTerminationCondition;
+import org.deeplearning4j.earlystopping.termination.MaxTimeIterationTerminationCondition;
+import org.deeplearning4j.earlystopping.trainer.EarlyStoppingTrainer;
 import org.deeplearning4j.nn.api.OptimizationAlgorithm;
+import org.deeplearning4j.nn.conf.MultiLayerConfiguration;
 import org.deeplearning4j.nn.conf.NeuralNetConfiguration;
 import org.deeplearning4j.nn.conf.layers.DenseLayer;
 import org.deeplearning4j.nn.conf.layers.OutputLayer;
@@ -22,7 +30,7 @@ import java.io.File;
 import java.io.IOException;
 import java.util.Collections;
 import java.util.List;
-import java.util.Random;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Created by User on 01-May-17.
@@ -32,10 +40,17 @@ public class ColumnChooser {
     private final int iterations = 30;
     private final double learningRate = 0.01;
     private MultiLayerNetwork net = null;
+    private List<BoardWinPair> boardWinPairs;
+    private MultiLayerConfiguration configuration;
+    private int height;
+    private int width;
 
     public ColumnChooser(List<BoardWinPair> boardWinPairs, int height, int width) {
-        this.net = createColumnChooser(boardWinPairs, height, width);
-        trainNN(boardWinPairs,height,width);
+        this.width = width;
+        this.height = height;
+        this.boardWinPairs = boardWinPairs;
+        createColumnChooser();
+        trainNN();
     }
 
     public ColumnChooser(File saved) {
@@ -57,14 +72,12 @@ public class ColumnChooser {
         }
     }
 
-    private MultiLayerNetwork createColumnChooser(List<BoardWinPair> boardWinPairs, int height, int width) {
+    private void createColumnChooser() {
         int totalSize = height * width;
         int numInput = totalSize;
         int nHidden = totalSize * 2;
         int numOutputs = 1;
-        MultiLayerNetwork net = null;
-        if (net == null) {
-            net = new MultiLayerNetwork(new NeuralNetConfiguration.Builder()
+        configuration = new NeuralNetConfiguration.Builder()
                     .seed(seed)
                     .iterations(iterations)
                     .optimizationAlgo(OptimizationAlgorithm.LBFGS)
@@ -85,10 +98,50 @@ public class ColumnChooser {
                     .layer(3, new OutputLayer.Builder(LossFunctions.LossFunction.MSE)
                             .activation(Activation.IDENTITY)
                             .nIn((nHidden / 3) * 2).nOut(numOutputs).build())
-                    .pretrain(false).backprop(true).build()
-            );
+                    .pretrain(false).backprop(true).build();
 
-            net.init();
+
+
+
+
+
+
+
+
+    }
+
+    private void trainNN() {
+        if(net == null){
+            double[][] inputArray = new double[boardWinPairs.size()][height * width];
+            double[][] outputArray = new double[boardWinPairs.size()][1];
+            for (int i = 0; i < inputArray.length; i++) {
+                inputArray[i] = boardWinPairs.get(i).getBoard();
+                outputArray[i] = new double[]{boardWinPairs.get(i).getOutcome()};
+            }
+
+            INDArray input = Nd4j.create(inputArray);
+            INDArray output = Nd4j.create(outputArray);
+
+            DataSet dataSet = new DataSet(input, output);
+            List<DataSet> list = dataSet.asList();
+            List<DataSet> trainData = list.subList(0, (list.size() / 3)*2);
+            List<DataSet> testData = list.subList((list.size() / 3)*2, list.size());
+            DataSetIterator myTrainData = new ListDataSetIterator(trainData, trainData.size());
+            DataSetIterator myTestData = new ListDataSetIterator(testData, testData.size());
+
+
+            EarlyStoppingConfiguration esConf = new EarlyStoppingConfiguration.Builder()
+                    .epochTerminationConditions(new MaxEpochsTerminationCondition(30))
+                    .iterationTerminationConditions(new MaxTimeIterationTerminationCondition(20, TimeUnit.MINUTES))
+                    .scoreCalculator(new DataSetLossCalculator(myTestData, true))
+                    .evaluateEveryNEpochs(1)
+                    .modelSaver(new LocalFileModelSaver(""))
+                    .build();
+
+            EarlyStoppingTrainer trainer = new EarlyStoppingTrainer(esConf, configuration, myTrainData);
+            EarlyStoppingResult result = trainer.fit();
+            net = (MultiLayerNetwork) result.getBestModel();
+
             net.setListeners(new ScoreIterationListener(1));
             double[] labelArray = new double[width];
             for (int i = 0; i < labelArray.length; i++) {
@@ -97,54 +150,28 @@ public class ColumnChooser {
             }
             INDArray labels = Nd4j.create(labelArray);
             net.setLabels(labels);
-
-
-            System.out.println("finished creating nn");
-
         }
 
-        return net;
 
 
     }
-    private void trainNN(List<BoardWinPair> boardWinPairs, int height, int width){
-        double[][] inputArray = new double[boardWinPairs.size()][height*width];
-        double[][] outputArray = new double[boardWinPairs.size()][1];
-        for (int i = 0; i < inputArray.length; i++) {
-            inputArray[i] = boardWinPairs.get(i).getBoard();
-            outputArray[i] = new double[]{boardWinPairs.get(i).getOutcome()};
-        }
 
-        INDArray input = Nd4j.create(inputArray);
-        INDArray output = Nd4j.create(outputArray);
-
-        DataSet dataSet = new DataSet(input, output);
-        List<DataSet> list = dataSet.asList();
-        Collections.shuffle(list);
-        Collections.shuffle(list);
-        Collections.shuffle(list);
-        DataSetIterator iterator = new ListDataSetIterator(list, boardWinPairs.size());
-        System.out.println("start training");
-        int epoch = 0;
-        do {
-            iterator.reset();
-            net.fit(iterator);
-
-            epoch++;
-        } while (epoch < iterations + 1);
-    }
-
-    public double chooseColumn(State gameState){
-        double[][] boardArray = new double[1][gameState.getH()*gameState.getW()];
+    public double chooseColumn(State gameState) {
+        double[][] boardArray = new double[1][gameState.getH() * gameState.getW()];
         boardArray[0] = gameState.convertToArray();
 
         INDArray input = Nd4j.create(boardArray);
-        INDArray output = net.output(input,false);
-        if(output.isScalar()){
-            return output.getDouble(0,0);
-        }else{
+        INDArray output = net.output(input, false);
+        if (output.isScalar()) {
+            return output.getDouble(0, 0);
+        } else {
             return -1.0;
         }
+
+    }
+
+    public void doTraining(BoardWinPair pair) {
+        boardWinPairs.add(pair);
 
     }
 }
